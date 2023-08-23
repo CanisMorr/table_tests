bea05808ed9bf777683b54c9815e270f463f1285
Batrakov <canis.morr@gmail.com>
2023-08-24 01:16:29 +0300

    Улучшил вывод данных

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 
+
 app = Flask(__name__)
 
 
@@ -10,7 +11,15 @@ def hello():
 @app.route('/test', methods=['GET', 'POST'])
 def testfunc():
     if request.method == 'POST':
-        #Тут мы получили POST, в который передались какие-то данные. Ответ дадим статический
+        #Тут мы получили POST, в который передались какие-то данные. Распарсим их у сунем в консоль
+        print ('Got POST.')
+        print ('id: ' + request.form['id'] )
+        print('name: ' + request.form['name'])
+        print('age: ' + request.form['age'])
+        print('col: ' + request.form['col'])
+        print('dob: ' + request.form['dob'])
+
+        # И дадим ответ, сейчас статический
         return '{"id":777,"name":"Новое Имя","age":"777","col":"Розовый","dob":"22/05/1982"}'
 
     else:
