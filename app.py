6494002316299d171c1d8b73a3edc2b1a98128dd
Batrakov <canis.morr@gmail.com>
2023-08-26 15:39:42 +0300

    Передача JSON, сформированного из строки

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ def testfunc():
     if request.method == 'POST':
         #Тут мы получили POST, в который передались какие-то данные. Распарсим их у сунем в консоль
         print ('Got POST.')
-        print ('id: ' + request.form['id'] )
-        print('name: ' + request.form['name'])
-        print('age: ' + request.form['age'])
-        print('col: ' + request.form['col'])
-        print('dob: ' + request.form['dob'])
+        #print ('id: ' + request.form['id'] )
+        #print('name: ' + request.form['name'])
+        #print('age: ' + request.form['age'])
+        #print('col: ' + request.form['col'])
+        #print('dob: ' + request.form['dob'])
+        print ('PERIODKEY: ' + request.form['PERIODKEY'])
 
         # И дадим ответ, сейчас статический
         otvet = '{"id":777,"name":"Новое Имя","age":"777","col":"Розовый","dob":"22/05/1982"}'
